52a79e0a129d1daef92fe5249aee22ac26f26d4a
Roberto Garcia <robertogarcia@ucsb.edu>
2022-11-09 01:35:43 -0500

    rg - minor modifications

--- pump/pump_system.cpp
+++ pump/pump_system.cpp
@@ -2,12 +2,12 @@
 // To have 2.25L of water processed, about 13.6 min is required. To have all the filters
 // used, a total of 40.9 min is needed. The system requires about 0.886 Ah of total charge.
 // At least 1Ah is recommended.
-#include <iostream>
-// for delay function.
-#include <chrono>
+
+
+#include <iostream>         // for debugging and testing
+#include <chrono>           // for delay function.
 #include <string>
 #include <thread>
-#include <cstdio>
 
 // for signal handling
 #include <JetsonGPIO.h>
@@ -18,6 +18,7 @@
 using namespace std;
 
 // BOARD Pin Definitions
+
 const int pump_pin = 12;
 const int but_pin = 18;   
 const int flow_pin = 16;
@@ -27,46 +28,24 @@ const int filter_3_pin = 15;
 
 // GLOBAL VARS
 
-static int user_input_filter = 0;
-static bool end_this_program = false;
-static volatile int pulse_count = 0;
-static volatile bool pump = 0;
+static int user_input_filter = 0;           // Used to activate appropriate filter to sample from. Need to specify!
+static volatile int pulse_count = 0;        // Number of pulses to determine amount of water sampled.
+static volatile double water_sampled = 0;   // Water measured in mL.
+static volatile bool pump = 0;              // State of the pump.
 
-// USEFULL FUNCTIONS
+// FUNCTIONS
 
 inline void delayMs(int ms) { this_thread::sleep_for(chrono::milliseconds(ms)); }
 
-void signalHandler(int s) { end_this_program = true; }
-
-// INTERRUPT FUNCTIONS
-
-// Toggles the pump. Outputs the measured amount of water when between on/off toggles.
-// Count is reset when
-void pumpToggle(const std::string& channel)
+// When CTRL+C pressed, signalHandler will be called. GPIO clean up
+void signalHandler(int s)
 {
-    
-    pump = !pump; // toggle state
-    if (pump) {
-        puts("PUMP ON");
-        pulse_count = 0;
-        GPIO::output(user_input_filter, GPIO::HIGH);
-        GPIO::output(pump_pin, GPIO::HIGH);
-    } else {
-        puts("PUMP OFF!");
-        GPIO::output(user_input_filter, GPIO::LOW);
-        GPIO::output(pump_pin, GPIO::LOW);
-        cout << pulse_count*0.223 << " mL" << endl;
-    }
-        
+    puts("Exiting");
+    GPIO::cleanup();
+    exit(0);
 }
 
-// ~4.467 pulses/mL -> 0.223 mL/pulse
-void flowHandler(const std::string& channel)
-{
-    pulse_count++;
-}
-
-// get an input for activating the filter to collect from
+// get an input for activating the filter to collect from, can be used in demo
 int getInputFilter()
 {
     static string input;
@@ -81,19 +60,47 @@ int getInputFilter()
         else if (input == "3")
                 return filter_3_pin;
 
-    } while (!end_this_program);
+    } while (1);
 
+}
+
+// INTERRUPT FUNCTIONS
+
+// Toggles the pump. Outputs the measured amount of water when between on/off toggles.
+// Count is reset when pump is activated.
+// Requires user_input_filter to be set to a filter
+void pumpToggle(const std::string& channel)
+{
+    
+    pump = !pump; // toggle state
+    if (pump) {
+        puts("PUMP ON");
+        pulse_count = 0;
+        GPIO::output(user_input_filter, GPIO::HIGH);
+        GPIO::output(pump_pin, GPIO::HIGH);
+    } else {
+        GPIO::output(user_input_filter, GPIO::LOW);
+        GPIO::output(pump_pin, GPIO::LOW);
+        puts("PUMP OFF!");
+        water_sampled = pulse_count*0.223;
+    }
+        
+}
+
+// ~4.467 pulses/mL -> 0.223 mL/pulse
+void flowHandler(const std::string& channel)
+{
+    pulse_count++;
 }
 
 int main()
 {
-    // When CTRL+C pressed, signalHandler will be called
     signal(SIGINT, signalHandler);
 
     // Pin Setup.
     GPIO::setmode(GPIO::BOARD);
 
-    // set pin as an output pin with optional initial state of HIGH
+    // set pins as inputs or outputs
     GPIO::setup(pump_pin, GPIO::OUT, GPIO::LOW);
     GPIO::setup(but_pin, GPIO::IN);
     GPIO::setup(flow_pin, GPIO::IN);
@@ -104,24 +111,20 @@ int main()
     cout << "Starting demo now! Press CTRL+C to exit" << endl;
 
     // detect events and trigger function
-    // GPIO::add_event_detect(but_pin, GPIO::Edge::RISING, pumpToggle, 20);
     GPIO::add_event_detect(flow_pin, GPIO::Edge::RISING, flowHandler, 0);
-    try {
-        while (!end_this_program)
-        {
 
-            user_input_filter = getInputFilter(); // get input from user until it's valid or program ends
-            GPIO::add_event_detect(but_pin, GPIO::Edge::RISING, pumpToggle, 200);
-            puts("Push button to start!");
-            while (pump == 0) {};
-            puts("Push button to end!");
-            while (pump == 1) {};
-            GPIO::remove_event_detect(but_pin);
-        }
-    } catch (exception& exc) {
-        cout << exc.what() << endl;
+    while (1)
+    {
+        user_input_filter = getInputFilter(); // get input from user until it's valid
+        GPIO::add_event_detect(but_pin, GPIO::Edge::RISING, pumpToggle, 200);
+        puts("Push button to start!");
+        while (pump == 0) {};
+        puts("Push button to end!");
+        while (pump == 1) {};
+        GPIO::remove_event_detect(but_pin);
+        cout << water_sampled << " mL" << endl;
     }
-    GPIO::cleanup();
+
 
     return 0;
 }

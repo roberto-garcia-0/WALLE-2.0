54ee264ac21360e2cac4258a6e269da881679bef
Roberto Garcia <robertogarcia@ucsb.edu>
2023-01-19 20:02:44 -0800

    rg - modified cam script and gitignore

--- cam/cam-test.py
+++ cam/cam-test.py
@@ -2,16 +2,18 @@ from multiprocessing import Process, Queue
 import cv2, time
 
   
-def capture_frames(src, video_file_name, syncQue, FPS):
+def capture_frames(src, video_file_name, syncQue, FPS, frameQue):
     capture = cv2.VideoCapture(src)
     cv2.VideoCapture()
     capture.set(cv2.CAP_PROP_BUFFERSIZE, 3)
+    capture.set(cv2.CAP_PROP_FPS, 30)
 
-    frame_width = int(capture.get(3))
-    frame_height = int(capture.get(4))
-    codec = cv2.VideoWriter_fourcc('M','J','P','G')
+    # frame_width = int(capture.get(3))
+    # frame_height = int(capture.get(4))
+    # codec = cv2.VideoWriter_fourcc('M','J','P','G')
+    # codec = cv2.VideoWriter_fourcc(*'mp4v')
     # To correcty set FPS, one must measure how many frames are recorded in a certain amount of time
-    output_video = cv2.VideoWriter(video_file_name + ".avi", codec, FPS,(frame_width,frame_height))
+    # output_video = cv2.VideoWriter(video_file_name + ".mp4", codec, FPS,(frame_width,frame_height))
 
     capture.read()
     syncQue.get()
@@ -34,8 +36,9 @@ def capture_frames(src, video_file_name, syncQue, FPS):
 
                 # Ensure valid frame
                 if status:
-                    cv2.imshow(src, frame)
-                    output_video.write(frame)
+                    # cv2.imshow(src, frame)
+                    frameQue.put(frame)
+                    # output_video.write(frame)
                 else:
                     break
                 # Press Q on window to stop
@@ -54,7 +57,22 @@ def capture_frames(src, video_file_name, syncQue, FPS):
         capture.release()
         cv2.destroyAllWindows()
 
+def combine_and_save_frames(frame1Que,frame2Que):
 
+    frame_width = 1920
+    frame_height = 1080*2
+    codec = cv2.VideoWriter_fourcc(*'mp4v')
+    output_video = cv2.VideoWriter("output.mp4", codec, 30,(frame_width,frame_height))
+    while True:
+        if (~frame1Que.empty() & ~frame2Que.empty()):
+            frame1 = frame1Que.get()
+            frame2 = frame2Que.get()
+
+            # Stack the frames horizontally
+            frames = cv2.vconcat([frame1, frame2])
+
+            # Write the frame to the file
+            output_video.write(frames)
 
 
 if __name__ == '__main__':
@@ -62,15 +80,20 @@ if __name__ == '__main__':
     # driver code
     # declare syncing que
     syncQue = Queue()
+
+    frame1Que = Queue()
+    frame2Que = Queue()
     # put x numbers in q based on number of cameras
     # this que will be used to have cameras wait on other cameras before getting another frame
     for i in range(2):
         print(i)
         syncQue.put(i)
     
-    P1 = Process(target=capture_frames, args=(('/dev/video2'),('cam1'),syncQue,28.6))
-    P2 = Process(target=capture_frames, args=(('/dev/video0'),('cam2'),syncQue,28.6))
+    P1 = Process(target=capture_frames, args=(('rtsp://192.168.0.250:554/h264'),('cam1'),syncQue,30,frame1Que))
+    P2 = Process(target=capture_frames, args=(('rtsp://192.168.0.251:554/h264'),('cam2'),syncQue,30,frame2Que))
 
     P1.start()
     P2.start()
+
+    combine_and_save_frames(frame1Que,frame2Que)
 

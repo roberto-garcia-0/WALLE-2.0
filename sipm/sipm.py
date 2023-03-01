94005d999baa73e5af3be420aa6d22cccaa596fd
Ebony Warren <ebony@169-231-181-40.wireless.ucsb.edu>
2023-03-01 13:14:46 -0800

    editing author

--- sipm/sipm.py
+++ sipm/sipm.py
@@ -5,10 +5,13 @@
 
 import time
 import ADS1263
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
-REF = 5.08          # Modify according to actual voltage
+REF = 5.00          # Modify according to actual voltage
                     # external AVDD and AVSS(Default), or internal 2.5V
+                    #VDD max = 7V
+                    #VSS min = -3V
+                    #max diff betwewen VDD and VSS = 7V, Min diff = -0.3V
 
 # ADC1 test part
 TEST_ADC1       = True
@@ -18,6 +21,60 @@ TEST_ADC1       = True
 TEST_ADC1_RATE   = False
 # RTD test part 
 # TEST_RTD        = False     
+
+#save time date
+#read data from port 0 and 4 to text doc
+#save time data
+#record data to new time doc
+
+# data format: 
+#yr/m/D:time:   port 0 Data  port 4 Data
+
+try:
+    ADC = ADS1263.ADS1263()
+    # The faster the rate, the worse the stability
+    # and the need to choose a suitable digital filter(REG_MODE1)
+    if (ADC.ADS1263_init_ADC1('ADS1263_38400SPS') == -1): #should return 0 if everthinng is working
+        exit()
+    ADC.ADS1263_SetMode(0) # 0 is singleChannel, 1 is diffChannel
+
+    ADC.ADS1263_ConfigADC()
+
+    
+    #main loop
+    channelList = [0, 4]
+
+    while(1):
+
+
+        ADC_Value = ADC.ADS1263_GetAll(channelList)    # get ADC1 value
+
+
+        for i in channelList:
+            if(ADC_Value[i]>>31 ==1):
+                print("ADC1 IN%d = -%lf" %(i, (REF*2 - ADC_Value[i] * REF / 0x80000000)))  
+            else:
+                print("ADC1 IN%d = %lf" %(i, (ADC_Value[i] * REF / 0x7fffffff)))   # 32bit
+        for i in channelList:
+            print("\33[2A")
+
+
+
+except IOError as e:
+    print(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 try:
     ADC = ADS1263.ADS1263()

53701968088a8aecfac5de88d5bbae9dc949db55
Roberto Garcia <robertogarcia@ucsb.edu>
2023-04-12 22:11:32 -0700

    rg - modifcations to main ctrl loop

--- cam/cams_recording.sh
+++ cam/cams_recording.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+# Modfiy so that gstreamer runs for a set amount of time, and takes it as a argument
 # Launch the gstreamer pipeline in the background
 gst-launch-1.0 -e rtspsrc location=rtsp://192.168.0.250:554/h264 ! rtph264depay ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw, width=720, height=480, format=BGRx ! m.sink_0 \
                rtspsrc location=rtsp://192.168.0.251:554/h264 ! rtph264depay ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw, width=720, height=480, format=BGRx ! m.sink_1 \
-               compositor name=m sink_0::xpos=0 sink_1::xpos=720 ! nvvidconv ! nvv4l2h264enc ! h264parse ! splitmuxsink location=./out_%02d.mp4 max-size-time=3600000000000 sync=true &
+               compositor name=m sink_0::xpos=0 sink_1::xpos=720 ! nvvidconv ! nvv4l2h264enc ! h264parse ! splitmuxsink location=./out_%02d.mp4 max-size-time=3600000000000 sync=true 
--- cam/control_loop.py
+++ cam/control_loop.py
@@ -1,54 +1,75 @@
 import subprocess
+import signal
+import time
 import RPi.GPIO as GPIO
 
-from pump_system import pump_system
+from pump/pump_system import pump_system
 
-class GPIO_Pins():
-    cam_state = 32
+# 1st Button Switch Crude Power On/Power Off
+# 2nd Button Toggle Camera Preview and Start Recording
+# This should require double rising edge detects
+# 3rd Graceful emergency shutoff
+# Should interrupt recording process while preserving current recording
 
-    video_duration = 31
-    video_duration_value = 1
+class smalle():
+    def __init__(self):
+    
+        # CONFIGURATION VARIABLES
+        self.video_duration = 12
+        self.pump_timestamps = [3,6,9]
+        self.use_pump_sys = True
+        self.use_sipm_sys = True
 
-# Press the external button to manually cycle through the video durations
-def increment_video_duration():
-    GPIO_Pins.video_duration_value = (GPIO_Pins.video_duration_value % 12) + 1
+        # SYSTEM VARIABLES
+        self.filters_sampled = 0
 
-# Sets up all of the GPIO pins required for the cam system
-def GPIO_setup():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(GPIO_Pins.cam_state, GPIO.IN)
-    GPIO.setup(GPIO_Pins.video_duration, GPIO.IN)
-    GPIO.add_event_detect(GPIO_Pins.video_duration, GPIO.RISING, callback = increment_video_duration, bouncetime = 0)
+
+        # PIN DEFINITION
+        self.cam_preview_toggle = 32
+        self.graceful_shutoff_toggle = 33
+
+        self.setUp()
+
+    # Sets up all of the GPIO pins required for the cam system
+    def setUp(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.cam_preview_toggle, GPIO.IN)
+        GPIO.setup(self.graceful_shutoff_toggle, GPIO.IN)
+        GPIO.add_event_detect(self.graceful_shutoff_toggle, GPIO.RISING, callback=self.gracefulShutoff, bouncetime=0)
+
+    def lightbeacon(self):
+        ## TODO
+
+    def gracefulShutoff(self):
+        ## TODO
+        exit(0)
+
+    def run(self):
+
+        # Preview - Use switch to exit and proceed to next state
+        preview_proc = subprocess.Popen(["./cams_preview.sh"])
+        GPIO.wait_for_edge(self.cam_preview_toggle, GPIO.RISING)
+        GPIO.wait_for_edge(self.cam_preview_toggle, GPIO.RISING)
+        preview_proc.send_signal(signal.SIGINT)
+
+        subprocess.run(["xset", "-display", ":0.0", "dpms", "force", "off"])
+        ## TODO: Subprocess to disable desktop environment
+
+        camera_proc = subprocess.Popen(["./cams_recording.sh"])
+        sipm_proc = subprocess.Popen([]) ## TODO: create callable SiPM python script
+        
+        sleep(60*60*pump_timestamps[1])
+        ## TODO: Pump system calls and sleeps
+
+        camera_proc.wait()
+        if sipm_proc.poll() is None:
+            sipm_proc.terminate()
+
+        self.lightbeacon()
+        GPIO.cleanup()
+
 
 # driver code
 if __name__ == '__main__':
-# small.e Setup
-    GPIO_setup()
-
-# Preview Mode
-    preview_proc = subprocess.Popen(["./cams_preview.sh"])
-    GPIO.wait_for_edge(GPIO_Pins.cam_state, GPIO.RISING)
-    preview_proc.terminate()
-    GPIO.remove_event_detect(GPIO_Pins.video_duration)
-    
-# Data Collection Mode
-
-    # Turn off display
-    subprocess.run(["xset", "-display", ":0.0", "dpms", "force", "off"])
-
-    # 
-    camera_proc = subprocess.Popen(["./cams_recording.sh"])
-    sipm_proc = subprocess.Popen([])
-    pump_proc = subprocess.Popen([])
-
-    camera_proc.wait()
-    if sipm_proc.poll() is None:
-        sipm_proc.terminate()
-    if pump_proc.poll() is None:
-        pump_proc.terminate()
-
-# small.e Cleanup
-    GPIO.cleanup()
-
-    # Activate beacon
-    subprocess.run()
+    s = smalle()
+    s.run()
